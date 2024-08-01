b543518d0172b34d13a2f36c4691b215fbc91eed
danieldelgado3 <156368433+danieldelgado3@users.noreply.github.com>
2024-08-01 15:29:56 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-client_id = '1f650dac452c409b806851e884681c66'
-client_secret = '7dc206b2a2b84c9388984f373f17552a'
-redirect_uri = 'http://localhost:5000/callback'
+client_id = 'YOUR-CLIENT-ID'
+client_secret = 'YOUR-CLIENT-SECRET'
+redirect_uri = 'YOUR-REDIRECT-URL'
 scope = 'user-library-read'
